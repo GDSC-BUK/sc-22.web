7cf78927f8cbbeaba829efd9df740785238d9eac
Horlakz <horlakz@protonmail.com>
2022-04-05 16:54:09 +0100

    created baselayout and auth pages, also finished register page

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,13 +1,16 @@
-import React from "react";
+import { Box } from "@chakra-ui/react";
+import BaseLayout from "../layouts/BaseLayout";
 
 function Index() {
   return (
-    <div>
-      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia minima
-      blanditiis quia ullam ut enim pariatur ratione ipsam autem soluta,
-      excepturi repellendus molestias eveniet consectetur tempore sint, eaque
-      quidem ex.
-    </div>
+    <BaseLayout>
+      <Box>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Officia minima
+        blanditiis quia ullam ut enim pariatur ratione ipsam autem soluta,
+        excepturi repellendus molestias eveniet consectetur tempore sint, eaque
+        quidem ex.
+      </Box>
+    </BaseLayout>
   );
 }
 
--- src/router/index.jsx
+++ src/router/index.jsx
@@ -1,12 +1,20 @@
 import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+// pages
 import Index from "../pages";
+import SignIn from "../pages/auth/SignIn";
+import SignUp from "../pages/auth/SignUp";
 
 function index() {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path="" element={<Index />} />
+        {/* home page */}
+        <Route path="/" element={<Index />} />
+
+        {/* auth pages */}
+        <Route path="/register" element={<SignUp />} />
+        <Route path="/login" element={<SignIn />} />
       </Routes>
     </BrowserRouter>
   );
