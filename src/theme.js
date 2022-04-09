32d07f6f5e748bb43a4808781c4d5564affb354b
Horlakz <horlakz@protonmail.com>
2022-04-09 22:55:20 +0100

    created theme file for the homepage colors

--- src/theme.js
+++ src/theme.js
@@ -2,7 +2,7 @@ import { extendTheme } from "@chakra-ui/react";
 
 const theme = extendTheme({
   colors: {
-    dashboardColor: {
+    homepageColor: {
       blue: "#497eb2",
       white: "#eaac00",
       yellow: "#f8e6c0",
