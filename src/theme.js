e92e23f5df4127290fc78584af5f86fbf344478f
Horlakz <horlakz@protonmail.com>
2022-04-09 22:54:30 +0100

    created theme file for the homepage colors

---
+++ src/theme.js
@@ -0,0 +1,15 @@
+import { extendTheme } from "@chakra-ui/react";
+
+const theme = extendTheme({
+  colors: {
+    dashboardColor: {
+      blue: "#497eb2",
+      white: "#eaac00",
+      yellow: "#f8e6c0",
+      green: "#4e8d53",
+      pink: "#ecaca2",
+    },
+  },
+});
+
+export default theme;
